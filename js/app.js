0170f821ec5f83e2745e33ae1229b8b81f20df3d
Jared Kranzler <kranzler312@gmail.com>
2018-06-15 18:09:58 -0500

    commit 1 Strings

---
+++ js/app.js
@@ -0,0 +1,101 @@
+// console.log('javascript is not the same as java')
+// weekend-js-1
+
+// Conceptual Questions
+
+// Long_Weekend covering loops, functions, and datatypes
+
+// 1. How do we assign a value to a variable?
+// 2. How do we change the value of a variable?
+// 3. How do we assign an existing variable to a new variable?
+
+
+
+
+// -------------
+// // Strings
+// -------------
+// 1. Create a variable called firstVariable.
+
+// • assign it the value of a string => "Hello World"
+
+// • change the value of this variable to a number.
+
+// • store the value of firstVariable into a new variable called 
+// secondVariable
+// --> completed thus far <--
+// • change the value of secondVariable to a string.
+
+// • What is the value of firstVariable?
+
+// 2. Create a variable called yourName and set it equal to your name as a string.
+
+// • Write an expression that takes the string "Hello, my name is " 
+// and the variable yourName so that it returns a new string with 
+// them concatenated.
+
+// • ex: Hello, my name is Jean Valjean
+
+
+let firstVariable = 'hello world'
+
+  firstVariable = 5;
+
+  // console.log(firstVariable);
+
+  secondVariable = firstVariable;
+
+console.log(secondVariable);
+
+secondVariable = "hello washington"
+
+console.log(firstVariable);
+// --------------------------------------------------
+//      The Value of the firt variable is 5.
+
+// --------------------------------------------------
+ const yourName = "Jared"
+  console.log('Hello, my name is ' + yourName + "!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------
+// // Booleans
+// -------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
