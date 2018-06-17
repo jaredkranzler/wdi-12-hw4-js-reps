a7466891501ba7e50d89ad35fd42cce7892ef8e4
Jared Kranzler <kranzler312@gmail.com>
2018-06-17 16:40:10 -0500

    up to reverse and calculat

--- js/app.js
+++ js/app.js
@@ -376,106 +376,150 @@
 // //----------------------------------------------------
 // //               What's in Your Closet?
 // //----------------------------------------------------
-const kristynsCloset = [
-  "left shoe",
-  "cowboy boots",
-  "right sock",
-  "GA hoodie",
-  "green pants",
-  "yellow knit hat",
-  "marshmallow peeps"
-];
+// const kristynsCloset = [
+//   "left shoe",
+//   "cowboy boots",
+//   "right sock",
+//   "GA hoodie",
+//   "green pants",
+//   "yellow knit hat",
+//   "marshmallow peeps"
+// ];
 
-// Thom's closet is more complicated. Check out this nested data structure!!
-const thomsCloset = [
-  [
-    // These are Thom's shirts
-    "grey button-up",
-    "dark grey button-up",
-    "light blue button-up",
-    "blue button-up",
-  ],[
-    // These are Thom's pants
-    "grey jeans",
-    "jeans",
-    "PJs"
-  ],[
-    // Thom's accessories
-    "wool mittens",
-    "wool scarf",
-    "raybans"
-  ]
-];
+// // Thom's closet is more complicated. Check out this nested data structure!!
+// const thomsCloset = [
+//   [
+//     // These are Thom's shirts
+//     "grey button-up",
+//     "dark grey button-up",
+//     "light blue button-up",
+//     "blue button-up",
+//   ],[
+//     // These are Thom's pants
+//     "grey jeans",
+//     "jeans",
+//     "PJs"
+//   ],[
+//     // Thom's accessories
+//     "wool mittens",
+//     "wool scarf",
+//     "raybans"
+//   ]
+// ];
 
-// //1. What's Kristyn wearing today? Using bracket notation to 
-// //access items in kristynsCloset, log the sentence 
-// "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
-console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!");
-
-
-// //2. Kristyn can't find her left shoe. Remove this item 
-// from her closet and save it to a variable named kristynShoe.
-const kristynShoe = kristynsCloset.shift()
-console.log(kristynsCloset);
-console.log(kristynShoe);
-
-// //3. Kristyn just bought some sweet shades! Add "raybans" to 
-// //   her closet after "yellow knit hat".
-kristynsCloset.splice(5, 0, "raybans");
-console.log(kristynsCloset);
-
-// //4. Kristyn spilled coffee on her hat... modify this 
-// //  item to read "stained knit hat" instead of yellow.
-kristynsCloset[4] = "stained knit hat";
-console.log(kristynsCloset);
-
-// //5. Put together an outfit for Thom! Using bracket 
-// // notation, access the first element in Thom's shirts array.
-console.log(thomsCloset[0][0]);
-
-// //6. In the same way, access one item from Thom's pants array.
-console.log(thomsCloset[1][0]);
-
-// //7. Access one item from Thom's accessories array.
-console.log(thomsCloset[2][2])
-
-// //8. Log a sentence about what Thom's wearing. Example: 
-// //"Thom is looking fierce in a grey button-up, jeans and wool scarf!"
-console.log("Thom is looking fierce in a "+thomsCloset[0][0] +" "+thomsCloset[1][1]+" & "+thomsCloset[2][0]+"!")
-
-// //9. Get more specific about what kind of PJs Thom's wearing 
-// // this winter. Modify the name of his PJ pants to Footie Pajamas.
-thomsCloset[2][2] = "Footie Pajamas";
-console.log(thomsCloset[2][2])
+// // //1. What's Kristyn wearing today? Using bracket notation to 
+// // //access items in kristynsCloset, log the sentence 
+// // "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+// console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!");
 
 
+// // //2. Kristyn can't find her left shoe. Remove this item 
+// // from her closet and save it to a variable named kristynShoe.
+// const kristynShoe = kristynsCloset.shift()
+// console.log(kristynsCloset);
+// console.log(kristynShoe);
+
+// // //3. Kristyn just bought some sweet shades! Add "raybans" to 
+// // //   her closet after "yellow knit hat".
+// kristynsCloset.splice(5, 0, "raybans");
+// console.log(kristynsCloset);
+
+// // //4. Kristyn spilled coffee on her hat... modify this 
+// // //  item to read "stained knit hat" instead of yellow.
+// kristynsCloset[4] = "stained knit hat";
+// console.log(kristynsCloset);
+
+// // //5. Put together an outfit for Thom! Using bracket 
+// // // notation, access the first element in Thom's shirts array.
+// console.log(thomsCloset[0][0]);
+
+// // //6. In the same way, access one item from Thom's pants array.
+// console.log(thomsCloset[1][0]);
+
+// // //7. Access one item from Thom's accessories array.
+// console.log(thomsCloset[2][2])
+
+// // //8. Log a sentence about what Thom's wearing. Example: 
+// // //"Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+// console.log("Thom is looking fierce in a "+thomsCloset[0][0] +" "+thomsCloset[1][1]+" & "+thomsCloset[2][0]+"!")
+
+// // //9. Get more specific about what kind of PJs Thom's wearing 
+// // // this winter. Modify the name of his PJ pants to Footie Pajamas.
+// thomsCloset[2][2] = "Footie Pajamas";
+// console.log(thomsCloset[2][2])
 
 
 
+// //--------------------------------------------------------------------
+// //                     Functions
+// //------------------------------------------------------------------
+// //                    printGreeting
+// //--------------------------------------------------------------------
+// //Write a function called printGreeting with a parameter name that 
+// //returns a greeting with the argument interpolated into the greeting.
+const name = 'Slimer';
+const printGreeting = (name) => {
+  return name;
+}
+console.log(`Hello there, ${name}!`);
+
+
+// //-----------------------------------------------------
+// //                 reverseWordOrder
+// //-----------------------------------------------------
+// //Write a function reverseWordOrder that accepts a single argument, 
+// //a string. The function should return a string with the order of 
+// //the words reversed. Don't worry about punctuation.
+
+const reverseWordOrder = (string) => {
+  const splitString = string.split(" ");
+  // console.log(splitString);
+  const reverseArray = splitString.reverse();
+  // console.log(reverseArray);
+  const joinArray = reverseArray.join(" ");
+  return joinArray;
+}
+console.log(reverseWordOrder("i'm into fitness... fitness whole pizza into my mouth"));
+
+
+// //---------------------------------------------------------
+// //                    calculate
+// //---------------------------------------------------------
+// //Write a function called calculate.
+
+// //This function should take three arguments, 
+// //two numbers and a string.
+
+// //Name the parameters num1, num2, and operation.
+
+// //If the function receives the string "add" for the 
+// //operation parameter, it will return the sum of num1 and num2.
+
+// //If the function receives the string "sub" for the 
+// //operation parameter, it will return num1 minus num2.
+
+// //Do the same thing for multiplication "mult", division 
+// //"div", and exponent "exp" (where num2 is the exponent of num1).
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const calculate = (num1, num2, operation) => {
+  if(operation === "add"){
+    return (num1 + num2);
+  } else if (operation === "sub"){
+    return (num1 - num2);
+  }else if (operation === "mult"){
+    return (num1 * num2);
+    }else if (operation === "div"){
+    return (num1 % num2);
+    }else 
+    return (Math.pow(num1, num2));
+}
+console.log(calculate(4, 3, "add"));
+console.log(calculate(4, 3, "sub"));
+console.log(calculate(40, 3, "mult"));
+console.log(calculate(40, 5, "div"));
+console.log(calculate(4, 3, "exp"));
 
 
 
