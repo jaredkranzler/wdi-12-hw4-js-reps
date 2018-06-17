a2984175b99a9dac5c009bce8dd57dff8dc16940
Jared Kranzler <kranzler312@gmail.com>
2018-06-16 22:46:19 -0500

    multiples of 3 & 5

--- js/app.js
+++ js/app.js
@@ -234,21 +234,76 @@
 
 // // 1.
 
-  let bankAccount = 0
- for (let x = 1; x <= 10; x++){ 
-  bankAccount = bankAccount + x;
-}
-console.log(bankAccount);
+//   let bankAccount = 0
+//  for (let x = 1; x <= 10; x++){ 
+//   bankAccount = bankAccount + x;
+// }
+// console.log(bankAccount);
 
-// // 2.
+// // // 2.
 
-  let bankAccount2 = 0
- for (let x = 1; x <= 100; x++){
-  bankAccount2 = bankAccount2 + (x*2);
-}
-console.log(bankAccount2);
+//   let bankAccount2 = 0
+//  for (let x = 1; x <= 100; x++){
+//   bankAccount2 = bankAccount2 + (x*2);
+// }
+// console.log(bankAccount2);
 
 // //---------------------------------------------------
+// //                Multiples of 3 && 5
+// //---------------------------------------------------
+
+
+// // 1. If we list all the natural numbers below 10 
+// //   that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+// //   The sum of these multiples is 23.
+// //   Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+let treeFive = 0
+ for (let x = 1; x < 1000; x++){
+  if (x % 3 === 0 || x % 5 === 0)
+    treeFive += x
+};
+console.log(treeFive);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
